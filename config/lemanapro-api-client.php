a68f958d5709b51652f5a984d927d621ed254a5d
Пархомей Алексей <parhomey@eurosvet.com>
2026-03-04 12:40:58 +0300

    feat config

--- config/lemanapro-api-client.php
+++ config/lemanapro-api-client.php
@@ -1,7 +1,29 @@
 <?php
 
 return [
-    'api_key' => env('LEMANAPRO_API_KEY'),
+    'environment' => env('LEMANAPRO_ENV', 'production'),
+
+    'environments' => [
+
+        'production' => [
+            'base_url' => 'https://api.lemanapro.ru:443',
+            'auth_url' => 'https://partners.auth.lemanapro.ru/realms/partner/protocol/openid-connect/token',
+        ],
+
+        'sandbox' => [
+            'base_url' => 'https://api-test.lemanapro.ru:443',
+            'auth_url' => 'https://partners.auth.lemanapro.ru/realms/partner/protocol/openid-connect/token',
+        ],
+
+        'debug' => [
+            'base_url' => 'http://localhost:8080',
+            'auth_url' => 'http://localhost:8081/token',
+        ],
+
+    ],
+
+    'client_id' => env('LEMANAPRO_CLIENT_ID'),
+    'client_secret' => env('LEMANAPRO_CLIENT_SECRET'),
 
     'http' => [
         'timeout' => 10,
