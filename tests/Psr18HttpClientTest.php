0d49cf198cbc64e3508f0cc93e98f1f6dc8c5431
Пархомей Алексей <parhomey@eurosvet.com>
2026-03-05 14:25:55 +0300

    Add unit test: Psr18HttpClientTest

---
+++ tests/Psr18HttpClientTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Escorp\LemanaProApiClient\Tests;
+
+use Http\Mock\Client as MockClient;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Escorp\LemanaProApiClient\Exceptions\LemanaProApiClientException;
+use Escorp\LemanaProApiClient\Exceptions\LemanaProHttpException;
+use Escorp\LemanaProApiClient\Http\Psr18HttpClient;
+use Escorp\LemanaProApiClient\Dto\LemanaProErrorDto;
+
+/**
+ *
+ */
+final class Psr18HttpClientTest extends TestCase
+{
+    private MockClient $mock;
+    private Psr18HttpClient $client;
+
+    protected function setUp(): void
+    {
+        $psr17 = new Psr17Factory();
+        $this->mock = new MockClient();
+        $this->client = new Psr18HttpClient($this->mock, $psr17, $psr17);
+    }
+
+    /** @test */
+    public function it_returns_decoded_json_on_success(): void
+    {
+        $this->mock->addResponse(new Response(200, [], json_encode(['ok' => true])));
+
+        $result = $this->client->request('GET', 'https://test');
+
+        $this->assertSame(['ok' => true], $result);
+    }
+
+    /** @test */
+    public function it_returns_empty_array_for_empty_body(): void
+    {
+        $this->mock->addResponse(new Response(200));
+
+        $result = $this->client->request('GET', 'https://test');
+
+        $this->assertSame([], $result);
+    }
+
+    /** @test */
+    public function it_throws_exception_on_invalid_json(): void
+    {
+        $this->mock->addResponse(new Response(200, [], '{broken'));
+
+        $this->expectException(LemanaProApiClientException::class);
+
+        $this->client->request('GET', 'https://test');
+    }
+
+    /** @test */
+    public function it_throws_wb_http_exception_on_json_error_response(): void
+    {
+        $error = [
+            'code' => 401,
+            'message' => 'Invalid key',
+        ];
+
+        $this->mock->addResponse(new Response(401, [], json_encode($error)));
+
+        try {
+            $this->client->request('GET', 'https://test');
+            $this->fail('Exception not thrown');
+        } catch (LemanaProHttpException $e) {
+            $this->assertInstanceOf(LemanaProErrorDto::class, $e->getError());
+            $this->assertSame(401, $e->getError()->code);
+        }
+    }
+
+    /** @test */
+    public function it_throws_api_exception_on_non_json_error_response(): void
+    {
+        $this->mock->addResponse(new Response(500, [], 'Internal error'));
+
+        $this->expectException(LemanaProApiClientException::class);
+
+        $this->client->request('GET', 'https://test');
+    }
+
+    /** @test */
+    public function it_throws_exception_on_conflicting_body_sources(): void
+    {
+        $this->expectException(\LogicException::class);
+
+        $this->client->request('POST', 'https://test', [
+            'json' => ['a' => 1],
+            'body' => 'raw'
+        ]);
+    }
+
+    /** @test */
+    public function it_builds_query_string_correctly(): void
+    {
+        $this->mock->addResponse(new Response(200, [], json_encode(['ok'=>1])));
+
+        $this->client->request('GET', 'https://test', [
+            'query' => ['from' => '2025-02-01']
+        ]);
+
+        $request = $this->mock->getLastRequest();
+
+        $this->assertStringContainsString('from=2025-02-01', (string)$request->getUri());
+    }
+
+    /** @test */
+    public function it_wraps_psr18_client_exception(): void
+    {
+        $this->mock->addException(new \RuntimeException('Transport error'));
+
+        $this->expectException(LemanaProApiClientException::class);
+
+        $this->client->request('GET', 'https://test');
+    }
+}
