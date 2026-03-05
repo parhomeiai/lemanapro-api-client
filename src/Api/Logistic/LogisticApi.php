c6c810918e4a9b4f389e120c122497dd10465b02
Пархомей Алексей <parhomey@eurosvet.com>
2026-03-05 17:18:47 +0300

    Feat LogisticApi: add method getLocationsBatch

--- src/Api/Logistic/LogisticApi.php
+++ src/Api/Logistic/LogisticApi.php
@@ -20,7 +20,7 @@ class LogisticApi extends AbstractLemanaProApi
      * @param int $perPage
      * @return LocationsResponse
      */
-    public function locations(int $page = 0, int $perPage = 20): LocationsResponse
+    public function getLocations(int $page = 0, int $perPage = 20): LocationsResponse
     {
         $url = $this->baseUrl . '/locations/logistic-location-public/v1/logistic-locations';
 
@@ -34,5 +34,34 @@ class LogisticApi extends AbstractLemanaProApi
         ]);
 
         return LocationsResponse::fromArray($response);
+    }
+
+    /**
+     * Возвращает весь список логистических локаций, соответствующих указанному владельцу
+     * @return array
+     */
+    public function getLocationsBatch(): array
+    {
+        $page = 0;
+        $perPage = 20;
+
+        $result = [];
+
+        do{
+            $response = $this->getLocations($page, $perPage);
+
+            if(empty($response->logisticLocations)){
+                break;
+            }
+
+            foreach ($response->logisticLocations as $logisticLocations) {
+                $result[] = $logisticLocations;
+            }
+
+            $page++;
+            usleep(600_000);
+        }while(true);
+
+        return $result;
     }
 }
